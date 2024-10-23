83017d811e0387c7eb397fda880e88ed65ccef59
Saul <saul@paymentwall.com>
2024-10-23 15:57:54 +0700

    Update Cover Photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,8 @@ const Header = function() {
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage: `url('https://i.ibb.co/GdQWRR0/IMG-20241023-150838.jpg')`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'top center'
         }}
       >
         {/* Optional overlay for better text visibility if needed */}
