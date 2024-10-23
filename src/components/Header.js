2379eace943d051d0a80b0c8cbdc2bf45566d6af
Saul <saul@paymentwall.com>
2024-10-23 16:47:59 +0700

    Update Cover Photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Header = function() {
   return (
     <div 
-      className="w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] bg-cover bg-top bg-no-repeat"
+      className="w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] bg-no-repeat bg-center"
       style={{
         backgroundImage: `url('https://i.ibb.co/GdQWRR0/IMG-20241023-150838.jpg')`,
+        backgroundSize: 'contain' // Change to 'contain' to ensure the image is fully visible
       }}
     />
   );
